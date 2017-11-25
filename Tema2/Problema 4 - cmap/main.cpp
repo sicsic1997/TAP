268f5692a14c671b098d134198fd7b0e07b1c7ad
Unknown <vladcoteanu1997@gmail.com>
2017-11-25 16:00:51 +0200

    Fixed cmap

--- Tema2/Problema 4 - cmap/main.cpp
+++ Tema2/Problema 4 - cmap/main.cpp
@@ -1,72 +1,133 @@
-#include<fstream>
-#include<iomanip>
-#include<algorithm>
-#include<math.h>
-#define INF 1000000001
-#define Nmax 100001
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <limits.h>
+#include <iomanip>
 using namespace std;
+
 ifstream f("cmap.in");
 ofstream g("cmap.out");
-struct punct{int x,y;} V[Nmax];
-struct solutie{int nod; double Dmax;} S;
-int N,p1,p2,A[Nmax];
-bool cmp(const punct &A,const punct &B)
-{
-    return A.x<B.x;
+
+
+struct point {
+
+    int x, y;
+
+};
+
+vector<point> tempPointList;
+vector<point> points, pointsX, pointsY;
+vector<point> leftY, rightY;
+
+bool cmpX(point A, point B) {
+    if(A.x - B.x)
+        return A.x < B.x;
+    return A.y < B.y;
 }
-bool cmp2(const int a,const int b)
-{
-    return V[a].x<V[b].x;
+
+bool cmpY(point A, point B) {
+    if(A.y - B.y)
+        return A.y < B.y;
+    return A.x < B.x;
 }
-void citire()
-{
-    f>>N;
-    for(int i=1;i<=N;++i) f>>V[i].x>>V[i].y;
+
+double getDist(point A, point B) {
+
+    return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
+
 }
-double dist(punct A,punct B)
-{
-    return sqrt(pow(B.x-A.x,2)+pow(B.y-A.y,2));
-}
-solutie calc(int st,int dr)
-{
-    if(st>=dr) //cazul in care am mai putin de un punct
-    {
-        solutie P;
-        P.Dmax=INF;
-        return P;
+
+vector<point> getInput() {
+
+    int numberOfPoints;
+    point temp;
+    vector<point> pointsList;
+    f >> numberOfPoints;
+    for(int i = 0; i < numberOfPoints; ++i) {
+
+        f >> temp.x >> temp.y;
+        pointsList.push_back(temp);
+
     }
-    if(dr-st<=2) // cazul in care am 2 sau 3 puncte
-    {
-        solutie P; P.Dmax=INF;
-        for(int i=st;i<=dr;++i)
-            for(int j=i+1;j<=dr;++j) if(dist(V[i],V[j])<P.Dmax) P.Dmax=dist(V[i],V[j]),P.nod=i;
-        return P;
-    }
-    //prelucrez solutiile din cele doua parti ale dreptei
-    int m=(st+dr)/2;
-    solutie P; P.Dmax=INF;
-    solutie P1=calc(st,m);
-    solutie P2=calc(m,dr);
-    // g<<P1.Dmax<<" "<<P2.Dmax<<'\n';
-    if(P1.Dmax<P.Dmax) P=P1;
-    if(P2.Dmax<P.Dmax) P=P2;
-    //prelucram sirul de care se situeaza la distanta maxima P.Dmax la stanga si la dreapta dreptei
-    int l=0;
-    for(int i=m;i>=st&&V[m].x-V[i].x<=P.Dmax;i--) A[++l]=i;
-    for(int i=m+1;i<=dr&&V[i].x-V[m].x<=P.Dmax;i++) A[++l]=i;
-    sort(A+1,A+l+1,cmp2);
-    for(int i=1;i<l;++i)
-        for(int j=i+1;j<=i+7&&j<=l;++j) if(dist(V[A[i]],V[A[j]])<P.Dmax) P.Dmax=dist(V[A[i]],V[A[j]]),P.nod=A[i];
-    //g<<st<<" * "<<dr<<" * "<<P.Dmax<<'\n';
-    return P;
+    return pointsList;
+
 }
+
+double getMinDist(int left, int right, vector<point> pointsY) {
+
+    /*for(int i = 0; i < pointsY.size(); ++i)
+        g << pointsY[i].x << "," << pointsY[i].y << " ";
+    g << '\n';*/
+
+    ///Single point case
+    if(left >= right) {
+
+        return INT_MAX;
+    }
+
+    ///2 or 3 points case
+    if(right - left <= 2) {
+
+        double temp = INT_MAX;
+        for(int i = left; i <= right; ++i)
+            for(int j = i + 1; j <= right; ++j)
+                temp = min(temp, getDist(points[i], points[j]));
+        return temp;
+
+    }
+
+    ///General case
+    int mid = (left + right) / 2;
+    double temp = INT_MAX, temp1, temp2;
+    leftY.clear();
+    rightY.clear();
+    for(int i = 0; i < pointsY.size(); ++i) {
+        if(pointsY[i].x <= pointsX[mid].x)
+            leftY.push_back(pointsY[i]);
+        if(pointsY[i].x >= pointsX[mid].x)
+            rightY.push_back(pointsY[i]);
+    }
+    /*for(int i = 0; i < leftY.size(); ++i)
+        g << leftY[i].x << "," << leftY[i].y << " ";
+    g << '\n';
+    for(int i = 0; i < rightY.size(); ++i)
+        g << rightY[i].x << "," << rightY[i].y << " ";
+    g << '\n';*/
+    ///Get the results from left and right side of the median
+    temp1 = getMinDist(left, mid, leftY);
+    temp2 = getMinDist(mid, right, rightY);
+    temp = min(temp1, temp2);
+
+    ///Get min distance for points on different side of the median
+    tempPointList.clear();
+    for(int i = 0; i < pointsY.size(); ++i)
+        if(pointsX[mid].x - pointsY[i].x <= temp)
+            tempPointList.push_back(pointsY[i]);
+
+    for(int i = 0; i < tempPointList.size(); ++i)
+        for(int j = i + 1; j < i + 7 && j < tempPointList.size(); ++j)
+            temp = min(temp, getDist(tempPointList[i], tempPointList[j]));
+
+    return temp;
+}
+
 int main()
 {
-    citire();
-    sort(V+1,V+N+1,cmp);
-    //  for(int i=1;i<=N;++i) g<<V[i].x<<" "<<V[i].y<<'\n';
-    S=calc(1,N);
-    g<<fixed<<setprecision(7)<<S.Dmax<<'\n';
-    // g<<S.nod<<'\n';
+    points = getInput();
+    pointsX = points;
+    pointsY = points;
+    sort(pointsX.begin(), pointsX.end(), cmpX);
+    sort(pointsY.begin(), pointsY.end(), cmpY);
+    /*for(int i = 0; i < pointsX.size(); ++i)
+        g << pointsX[i].x << " ";
+    g << '\n';
+    for(int i = 0; i < pointsY.size(); ++i)
+        g << pointsY[i].y << " ";
+    g << '\n';*/
+    double minDist = getMinDist(0, points.size() - 1, pointsY);
+
+    g << setprecision(20) << minDist << '\n';
+
     return 0;
 }
